1a1b2d1feb1e4d86baa65f71c824309f55e6d2f5
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-07-06 01:54:35 +0300

    c

--- js/common.js
+++ js/common.js
@@ -2,12 +2,16 @@ let pollLogic = {
   btns: document.querySelectorAll('.btn'),
   mainScreen: document.querySelector('.main-screen'),
   pollSect: document.querySelectorAll('.poll-item'),
+  pollWrap: document.querySelector('.poll-wrap'),
+  progressBar: document.querySelector('.determinate'),
 
   init () {
     let self = this;
+    this.pollWrap.style.display = 'none'
     this.showPollSections ();
     this.btns.forEach(function(btn, i, arr) {
       btn.addEventListener('click', e => {
+          self.pollWrap.style.display = 'block'
           self.mainScreen.style.display = 'none';
           self.getNextSection(i)
       })
@@ -19,8 +23,10 @@ let pollLogic = {
     })
   },
   getNextSection (i) {
-    this.showPollSections()
-    this.pollSect[i].style.display = 'block'
+    this.showPollSections();
+    this.progressBar.style.width = 100 / (this.pollSect.length - 1) * i + '%';
+    this.pollSect[i].style.display = 'block';
+    this.pollWrap.style.height = this.pollSect[i].offsetHeight + 100 + 'px'
   }
 }
 pollLogic.init()
